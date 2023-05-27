50fc3dd9931153f369650010e774c676a0aa7c84
Arvin Poddar <arvin.poddar@gmail.com>
2023-05-27 19:44:04 -0400

    Update base css, redesign homepage

--- assets/js/Ioda/components/loading/Loading.js
+++ assets/js/Ioda/components/loading/Loading.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import {Link} from "react-router-dom";
-import T from 'i18n-react';
-
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
+import T from "i18n-react";
 
 class Loading extends Component {
-    render() {
-        return (
-            <div className="progress-bar-striped">
-                <div style={{width: "100%"}}>
-                    <strong>
-                        {
-                            this.props.text ? <p>{this.props.text}</p> : <T.p text="loadingBar.loading"/>
-                        }
-                    </strong>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="progress-bar-striped">
+        <div style={{ width: "100%" }}>
+          <strong>
+            {this.props.text ? (
+              <p>{this.props.text}</p>
+            ) : (
+              <T.p text="loadingBar.loading" />
+            )}
+          </strong>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Loading;
