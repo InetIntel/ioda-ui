8975a57d4f8d33b3662b1627d5de337a435b9d89
Arvin Poddar <arvin.poddar@gmail.com>
2023-06-15 16:40:55 -0400

    Layout fixes on Entity Page

--- assets/js/Ioda/components/tooltip/Tooltip.js
+++ assets/js/Ioda/components/tooltip/Tooltip.js
@@ -2,32 +2,29 @@ import React, { Component } from "react";
 import { Button, Popover } from "antd";
 import { QuestionCircleOutlined } from "@ant-design/icons";
 
-class Tooltip extends Component {
-  render() {
-    const { title, text, customCode } = this.props;
-
-    return (
-      <Popover
-        placement="right"
-        title={title}
-        content={customCode ?? text}
-        trigger="hover"
-        overlayStyle={{
-          maxWidth: "275px",
-        }}
-        overlayClassName="ioda-help-tooltip"
-        color="rgba(0, 0, 0, 0.9)"
-      >
-        <Button
-          type="link"
-          color="primary"
-          shape="circle"
-          icon={<QuestionCircleOutlined />}
-          size="small"
-        />
-      </Popover>
-    );
-  }
-}
+const Tooltip = ({ title, text, customCode, className }) => {
+  return (
+    <Popover
+      placement="right"
+      title={title}
+      content={customCode ?? text}
+      trigger="hover"
+      overlayStyle={{
+        maxWidth: "275px",
+      }}
+      overlayClassName="ioda-help-tooltip"
+      color="rgba(0, 0, 0, 0.9)"
+    >
+      <Button
+        className={className}
+        type="link"
+        color="primary"
+        shape="circle"
+        icon={<QuestionCircleOutlined />}
+        size="small"
+      />
+    </Popover>
+  );
+};
 
 export default Tooltip;
