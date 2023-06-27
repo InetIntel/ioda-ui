3672961258286b575480d38131bdee2496a1490e
Arvin Poddar <arvin.poddar@gmail.com>
2023-06-27 02:51:23 -0400

    feat: add map legend

--- assets/js/Ioda/utils/mapColors.js
+++ assets/js/Ioda/utils/mapColors.js
@@ -1,5 +1,23 @@
 import d3 from "d3";
 
+export const LOW_COLOR = "#FFCC3D";
+export const HIGH_COLOR = "#EE695B";
+export const MAX_COLOR = "#EA5362";
+
+export const getThresholdBoundsForCountry = () => {
+  return {
+    low: 1_200,
+    high: 29_000_000,
+  };
+};
+
+export const getThresholdBoundsForRegion = () => {
+  return {
+    low: 5_000,
+    high: 1_000_000,
+  };
+};
+
 export const getEntityScaleColor = (score, entityType = "country") => {
   if (entityType === "country") {
     return getCountryScaleColor(score);
@@ -16,10 +34,11 @@ export const getCountryScaleColor = (score) => {
   }
 
   if (score > 29_000_000) {
-    return "#EA5362";
+    return MAX_COLOR;
   }
 
-  return getColorFromLinearScale(score, 1_200, 29_000_000);
+  const { low, high } = getThresholdBoundsForCountry();
+  return getColorFromLinearScale(score, low, high);
 };
 
 export const getRegionScaleColor = (score) => {
@@ -28,18 +47,19 @@ export const getRegionScaleColor = (score) => {
   }
 
   if (score > 1_000_000) {
-    return "#EA5362";
+    return MAX_COLOR;
   }
 
-  return getColorFromLinearScale(score, 5_000, 1_000_000);
+  const { low, high } = getThresholdBoundsForRegion();
+  return getColorFromLinearScale(score, low, high);
 };
 
 export const getColorFromLinearScale = (
   score,
   minScore,
   maxScore,
-  minColor = "#FFCC3D",
-  maxColor = "#EE695B"
+  minColor = LOW_COLOR,
+  maxColor = HIGH_COLOR
 ) => {
   const linearScale = d3.scale
     .linear()
