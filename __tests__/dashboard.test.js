5460544e34bd83f21c5090745c09ff677a2fbbec
ydaniju <yusuf.daniju@doctolib.com>
2024-02-14 18:15:25 +0100

    setup basic guardrails

--- __tests__/dashboard.test.js
+++ __tests__/dashboard.test.js
@@ -15,22 +15,6 @@ describe("on page load", () => {
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
-    // Type into search box
-    // await page.type(".devsite-search-field", "automate beyond recorder");
-
-    // // Wait and click on first result
-    // const searchResultSelector = ".devsite-result-item-link";
-    // await page.waitForSelector(searchResultSelector);
-    // await page.click(searchResultSelector);
-
-    // // Locate the full title with a unique string
-    // const textSelector = await page.waitForSelector(
-    //   "text/Customize and automate"
-    // );
-    // const fullTitle = await textSelector?.evaluate((el) => el.textContent);
-
-    // // Print the full title
-    // console.log('The title of this blog post is "%s".', fullTitle);
     const image = await page.screenshot();
 
     expect(image).toMatchImageSnapshot({
